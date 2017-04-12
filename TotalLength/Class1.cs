eea4f2aa2df860a25cbb9a51ebc3662f833e6fee
Hans Palacios <hans.palacios@hok.com>
2017-04-12 10:44:22 -0500

    Initial Commit.  Test workflow from archi-lab guide.

---
+++ TotalLength/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace TotalLength
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CurveTotalLength : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            throw new NotImplementedException();
+
+            // Get application and document objects
+
+            try
+            {
+
+                // Implement Selection Filter to select curves
+
+                // Measure their total length
+
+                // Return a message window that displays total length to user
+
+                // Assuming that everything went right return Result.Succeeded
+                return Result.Succeeded;
+            }
+            // This is where we "catch" potential errors and define how to deal with
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                // If user decided to cancel the operation return Result.Canceled
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                // If something went wrong return Result.Failed
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
